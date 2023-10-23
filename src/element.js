74dde1c39d3f1b1aa615cebc4b3fc6e00d4284ad
yukapril <i@yukapril.com>
2023-10-23 16:17:39 +0800

    fix: 每次复用相同输入框，解决 iOS 移除元素后收不到回调问题

--- src/element.js
+++ src/element.js
@@ -29,20 +29,29 @@ export const inputTag = (options) => {
   }
 
   const create = () => {
-    inputEl = document.createElement('input')
-    inputEl.type = 'file'
-    inputEl.style.opacity = '0'
-    inputEl.style.width = '0'
-    inputEl.style.height = '0'
-    inputEl.setAttribute('accept', 'image/*')
-    if (cap) inputEl.setAttribute('capture', cap)
-    document.body.appendChild(inputEl)
+    inputEl = document.querySelector('#CameraH5Input')
 
-    inputEl.addEventListener('click', () => {
-      remove()
-    })
+    if (inputEl) {
+      if (cap) inputEl.setAttribute('capture', cap)
+    } else {
+      inputEl = document.createElement('input')
+      inputEl.id = 'CameraH5Input'
+      inputEl.type = 'file'
+      inputEl.style.opacity = '0'
+      inputEl.style.width = '0'
+      inputEl.style.height = '0'
+      inputEl.setAttribute('accept', 'image/*')
+      if (cap) inputEl.setAttribute('capture', cap)
+      document.body.appendChild(inputEl)
+    }
+
+    // 不能点击后移除元素，否则 iOS 会收不到回调事件。缺点是如果用户选择相册/拍照，点击了取消，此时元素不会移除。
+    // inputEl.addEventListener('click', () => {
+    // remove()
+    // })
 
     inputEl.addEventListener('change', e => {
+      console.log('[camera-h5]', '图片发生改变')
       options.change(e)
 
       const files = e.target.files
@@ -51,15 +60,21 @@ export const inputTag = (options) => {
         const fileType = file.type
         if (!checkType(options.type, fileType)) {
           const err = 'Illegal type: ' + fileType
+          console.log('[camera-h5]', '图片改变完成，checkTypeErr：', err)
           options.changed({ type: 'type', err }, '', null)
+          remove()
           return
         }
         fileReader(file, (error, base64) => {
           if (error) {
+            console.log('[camera-h5]', '图片改变完成，fileReaderErr：', error)
             options.changed({ type: 'error file', err: 'Illegal file:' + file, error }, '', null)
+            remove()
             return
           }
+          console.log('[camera-h5]', '图片改变完成，success')
           options.changed(null, base64, file)
+          remove()
         })
       }
     })
